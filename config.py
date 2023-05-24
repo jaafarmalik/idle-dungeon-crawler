3bbb12c80d4a53d5c0c689ad5026205a4887e394
Jaafar Al-Maliky <79025198+jaafarmalik@users.noreply.github.com>
2023-05-24 14:42:54 -0400

    Implenting New Jekyll Template

---
+++ config.py
@@ -0,0 +1,8 @@
+## 	Idle Dungeon Crawler        |
+## CONFIG SETTINGS - config.py  |
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+
+TOKEN = "MTEwOTk3MzQzMDM2MDYwNDc5Mw.GWXNSd.NG3YGiVbS1TwMs3uIN8d6IaPkXEDXYRC0ftvF8"
+
+CURRENT_REV = "0.0.0.2"
+DEBUG = True
